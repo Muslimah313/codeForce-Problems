af93d7fa0bef5e7cf74f69871d27aa9598565818
Muslimah313 <ummati300@gmail.com>
2025-10-28 06:06:45 +0600

    Add files via upload

---
+++ sheet1b.ccp.c
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    long long l;
+    char c;
+    float f;
+    double d;
+
+    cin >> i >> l >> c >> f >> d;
+    cout << i << "\n" << l << "\n" << c << "\n" << f << "\n" << d;
+
+    return 0;
+}
+
